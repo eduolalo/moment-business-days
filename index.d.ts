7eee4c9f375097cc43d513d798def6fcd9bb09a1
David Gasperoni <mcdado@gmail.com>
2018-06-25 15:03:26 +0200

    Add types declarations file

---
+++ index.d.ts
@@ -0,0 +1,27 @@
+import * as moment from 'moment';
+
+declare module 'moment' {
+    interface Moment {
+        isHoliday: () => boolean;
+        isBusinessDay: () => boolean;
+
+        businessDaysIntoMonth: () => Moment;
+
+        businessDiff: (param: Moment) => number;
+        businessAdd: (param: number, period?: unitOfTime.Base) => Moment;
+        businessSubtract: (param: number, period?: unitOfTime.Base) => Moment;
+
+        nextBusinessDay: () => Moment;
+        prevBusinessDay: () => Moment;
+
+        monthBusinessDays: (partialEndDate?: Moment) => Moment[];
+        monthNaturalDays: (fromToday?: boolean) => Moment[];
+        monthBusinessWeeks: (fromToday?: boolean) => Moment[][];
+        monthNaturalWeeks: (fromToday?: boolean) => Moment[][];
+    }
+
+    interface LocaleSpecification {
+        holidays?: string[];
+        holidayFormat?: string;
+    }
+}
