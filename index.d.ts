50c77020feff2dd463543115bb0fa1e311a3a79c
David Gasperoni <mcdado@gmail.com>
2018-11-16 19:19:40 +0100

    Improved types definitions file

--- index.d.ts
+++ index.d.ts
@@ -2,22 +2,22 @@ import * as moment from 'moment';
 
 declare module 'moment' {
     interface Moment {
-        isHoliday: () => boolean;
-        isBusinessDay: () => boolean;
+        isHoliday(): boolean;
+        isBusinessDay(): boolean;
 
-        businessDaysIntoMonth: () => number;
+        businessDaysIntoMonth(): number;
 
-        businessDiff: (param: Moment) => number;
-        businessAdd: (param: number, period?: unitOfTime.Base) => Moment;
-        businessSubtract: (param: number, period?: unitOfTime.Base) => Moment;
+        businessDiff(param: Moment): number;
+        businessAdd(param: number, period?: unitOfTime.Base): Moment;
+        businessSubtract(param: number, period?: unitOfTime.Base): Moment;
 
-        nextBusinessDay: () => Moment;
-        prevBusinessDay: () => Moment;
+        nextBusinessDay(): Moment;
+        prevBusinessDay(): Moment;
 
-        monthBusinessDays: (partialEndDate?: Moment) => Moment[];
-        monthNaturalDays: (fromToday?: boolean) => Moment[];
-        monthBusinessWeeks: (fromToday?: boolean) => Moment[][];
-        monthNaturalWeeks: (fromToday?: boolean) => Moment[][];
+        monthBusinessDays(partialEndDate?: Moment): Moment[];
+        monthNaturalDays(fromToday?: boolean): Moment[];
+        monthBusinessWeeks(fromToday?: boolean): Moment[][];
+        monthNaturalWeeks(fromToday?: boolean): Moment[][];
     }
 
     interface LocaleSpecification {
