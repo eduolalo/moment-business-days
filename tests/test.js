eduolalo/moment-business-days

commit 49477efd2a78abc51b2524cf9fc224f7c6f13bdd

Fix infinite loop caused by fraction value in businessAdd
Fixes #55

* Fix: Fractional day causes infinite loop in businessAdd #55
* Add absolute number rounding in businessAdd
* Fix tests for fractional days

index.js
@@ -71,15 +71,22 @@ moment.fn.businessDiff = function (param) {
 };
 
 moment.fn.businessAdd = function (number, period) {
-  var day = this.clone();
+  var day = this.clone().startOf('day');
   if (!day.isValid()) {
     return day;
   }
+
+  if (number < 0) {
+    number = Math.round(-1 * number) * -1;
+  } else {
+    number = Math.round(number);
+  }
+
   var signal = number < 0 ? -1 : 1;
-  var remaining = Math.abs(number);
   period = typeof period !== 'undefined' ? period : 'days';
 
-  while (remaining) {
+  var remaining = Math.abs(number);
+  while (remaining > 0) {
     day.add(signal, period);
 
     if (day.isBusinessDay()) {

tests/test.js
@@ -151,17 +151,37 @@ describe('Moment Business Days', function () {
       });
     });
     describe('On Tuesday, November 3rd 2015', function () {
-      it('adds business days only, excluding weekends, even over 2 weeks ', function (done) {
+      it('adds business days only, excluding weekends, even over 2 weeks', function (done) {
         var newBusinessDay = moment('11-03-2015', 'MM-DD-YYYY').businessAdd(5);
         expect(newBusinessDay.format('D')).to.eql('10');
         done();
       });
-      it('adds business days only, excluding weekends ', function (done) {
+      it('adds business days only, excluding weekends', function (done) {
         var newBusinessDay = moment('11-03-2015', 'MM-DD-YYYY').businessAdd(10);
         expect(newBusinessDay.format('D')).to.eql('17');
         done();
       });
-      it('adds business hours only, excluding weekends ', function (done) {
+      it('adds business days only, excluding weekends, rounding down fractional day values', function (done) {
+        var newBusinessDay = moment('11-03-2015 12:42:00', 'MM-DD-YYYY hh-mm-ss').businessAdd(10.4);
+        expect(newBusinessDay.format('D')).to.eql('17');
+        done();
+      });
+      it('adds business days only, excluding weekends, rounding up fractional day values', function (done) {
+        var newBusinessDay = moment('11-03-2015 12:42:00', 'MM-DD-YYYY hh-mm-ss').businessAdd(10.5);
+        expect(newBusinessDay.format('D')).to.eql('18');
+        done();
+      });
+      it('subtracts business days when negative values are added, excluding weekends, rounding down fractional day values', function (done) {
+        var newBusinessDay = moment('11-03-2015 12:42:00', 'MM-DD-YYYY hh-mm-ss').businessAdd(-10.4);
+        expect(newBusinessDay.format('D')).to.eql('20');
+        done();
+      });
+      it('subtracts business days when negative values are added, excluding weekends, rounding up fractional day values', function (done) {
+        var newBusinessDay = moment('11-03-2015 12:42:00', 'MM-DD-YYYY hh-mm-ss').businessAdd(-10.5);
+        expect(newBusinessDay.format('D')).to.eql('19');
+        done();
+      });
+      it('adds business hours only, excluding weekends', function (done) {
         var newBusinessDay = moment('11-06-2015', 'MM-DD-YYYY').businessAdd(
           36,
           'hours'

commit 6d2f1f502f4528912fd09caf895dc5f2a22588af

Fix regression introduced by #59
Using businessAdd does not reset the time of day.

index.js
@@ -71,7 +71,7 @@ moment.fn.businessDiff = function (param) {
 };
 
 moment.fn.businessAdd = function (number, period) {
-  var day = this.clone().startOf('day');
+  var day = this.clone();
   if (!day.isValid()) {
     return day;
   }

tests/test.js
@@ -161,6 +161,11 @@ describe('Moment Business Days', function () {
         expect(newBusinessDay.format('D')).to.eql('17');
         done();
       });
+      it('adds business days only, excluding weekends, preserving time-of-day', function (done) {
+        var newBusinessDay = moment('11-03-2015 12:42:00', 'MM-DD-YYYY hh-mm-ss').businessAdd(10);
+        expect(newBusinessDay.format('MM-DD-YYYY hh:mm:ss')).to.eql('11-17-2015 12:42:00');
+        done();
+      });
       it('adds business days only, excluding weekends, rounding down fractional day values', function (done) {
         var newBusinessDay = moment('11-03-2015 12:42:00', 'MM-DD-YYYY hh-mm-ss').businessAdd(10.4);
         expect(newBusinessDay.format('D')).to.eql('17');